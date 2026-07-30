7079bd5e26d464445837a8c4a64e1ca584ed5ae8
gunbayz <gunbayz@gmail.com>
2026-07-30 19:59:28 -0400

    Button fix 2
    Fixing the iOS bug that was messing up button positioning

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'today-tomorrow-v13';
+const CACHE = 'today-tomorrow-v14';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
