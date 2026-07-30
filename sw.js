79e93d7aac3111f142e9525ec03b447e8634870c
gunbayz <gunbayz@gmail.com>
2026-07-30 19:50:51 -0400

    Fixing button positions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'today-tomorrow-v12';
+const CACHE = 'today-tomorrow-v13';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
