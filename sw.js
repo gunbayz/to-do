83f8ac2280a8c11c20100423dbbfb2c3fd5c9904
gunbayz <gunbayz@gmail.com>
2026-07-30 21:58:52 -0400

    arrow alignment fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'today-tomorrow-v24';
+const CACHE = 'today-tomorrow-v25';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
