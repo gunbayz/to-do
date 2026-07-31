f20e80d5cbd540312b918f0673dc6313e7cb89c3
gunbayz <gunbayz@gmail.com>
2026-07-30 21:23:18 -0400

    Bug fixes
    switch between today and tomorrow, and press and hold to edit fixes (hopefully).

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'today-tomorrow-v21';
+const CACHE = 'today-tomorrow-v22';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
