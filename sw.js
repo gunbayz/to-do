5bea627e5eb39f27ae36e703d3a42f6112d1d74e
gunbayz <gunbayz@gmail.com>
2026-09-16 03:58:50 +0000

    Revert "Yellow theme, new lemon icons, change clear button"
    This reverts commit 70fa5b36698b1f3888455852e5cdf509b5e786f6.

--- sw.js
+++ sw.js
@@ -1,30 +1,25 @@
-var CACHE = 'bakkal-v2';
-var ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png', './favicon.ico', './favicon.png'];
+const CACHE = 'today-tomorrow-v31';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png', './favicon.png'];
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }));
-  self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
-    }).then(function () { return self.clients.claim(); })
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener('fetch', function (e) {
-  if (e.request.method !== 'GET') return;
+self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).then(function (res) {
-      var copy = res.clone();
-      caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
-      return res;
-    }).catch(function () {
-      return caches.match(e.request, { ignoreSearch: true }).then(function (r) {
-        return r || caches.match('./index.html');
-      });
-    })
+    caches.match(e.request, { ignoreSearch: true }).then(hit =>
+      hit || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      }).catch(() => {
+        if (e.request.mode === 'navigate') return caches.match('./index.html');
+      })
+    )
   );
 });
